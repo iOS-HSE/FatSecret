f8f1413d45495fb7ab23b53166bb17e8c1841aeb
user184905 <user184905@FF065.local>
2020-12-07 18:09:55 +0100

    feat/add collection view

---
+++ FatSecret/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  FatSecret
+//
+//  Created by user184905 on 12/7/20.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+}
---
+++ FatSecret/FoodViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  FoodViewCell.swift
+//  FatSecret
+//
+//  Created by user184905 on 12/7/20.
+//
+
+import UIKit
+
+class FoodViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    
+}
